ecc3097e3292b12944955507b25dcfbe08755f1d
Hyye <100793429+Hyye123@users.noreply.github.com>
2022-11-20 14:12:30 +0300

    Update MPP Custom Tags.user.js

--- MPP Custom Tags.user.js
+++ MPP Custom Tags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MPP Custom Tags
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.5
 // @description  MPP Custom Tags (MPPCT)
 // @author       НУУЕ (!НУУЕ!#4440)
 // @match        *://mppclone.com/*
@@ -10,43 +10,54 @@
 // @license      MIT
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mppclone.com
 // ==/UserScript==
- 
- 
+
+
 console.log('Loaded MPPCT.')
 if (!localStorage.tag) {
     localStorage.tag = JSON.stringify({text: "USER", color: "#000000"});
 }
-const ver = '1.0.4';
+const ver = '1.0.5';
 var tag = JSON.parse(localStorage.tag);
- 
+
 MPP.client.on('hi', () => {
     if (!MPP.client.customSubscribed) {
         MPP.client.sendArray([{m:"+custom"}]);
         MPP.client.customSubscribed = true;
     }
 });
- 
- 
+
+var bufferlocked = false;
+function sendTag() {
+    if (bufferlocked) return;
+    MPP.client.sendArray([{m: "custom", data: {m: 'mppct', tag: tag.text, color: tag.color}, target: { mode: 'subscribed', global: false } }])
+    bufferlocked = true;
+    setTimeout(function() {
+        bufferlocked = false;
+    }, 2500)
+}
+
 function hexToRGB(hex) {
     let r = 0, g = 0, b = 0;
- 
+
     r = "0x" + hex[1] + hex[2];
     g = "0x" + hex[3] + hex[4];
     b = "0x" + hex[5] + hex[6];
- 
+
     var Rgb = +r + ', ' + +g + ', ' + +b;
     return Rgb;
 }
- 
+
 MPP.client.on("custom", (data) => {
     if (data.data.m == 'mppct') {
         if (data.data.tag && data.data.color) {
-            document.getElementById(`namediv-${data.p}`).innerHTML = `<div class="nametag" id="nametag-${data.p}" style="background-color: rgb(${hexToRGB(data.data.color)});">${data.data.tag}</div><div class="nametext" id="nametext-${data.p}">${MPP.client.findParticipantById(data.p).name}</div>`;
-            document.getElementById(`namediv-${data.p}`).title = "This is a MPPCT user."
+            if (MPP.client.ppl[data.p] != undefined) {
+                document.getElementById(`namediv-${data.p}`).innerHTML = `<div class="nametag" id="nametag-${data.p}" style="background-color: rgb(${hexToRGB(data.data.color)});">${data.data.tag}</div><div class="nametext" id="nametext-${data.p}">${MPP.client.findParticipantById(data.p).name}</div>`;
+                document.getElementById(`namediv-${data.p}`).title = "This is a MPPCT user.";
+            }
         }
     }
     if (data.data.m == 'mppctgt') {
-        MPP.client.sendArray([{m: "custom", data: {m: 'mppct', tag: tag.text, color: tag.color}, target: { mode: 'subscribed', global: false } }]);
+        sendTag();
     }
 });
 MPP.client.on("p", (p) => {
@@ -54,7 +65,7 @@ MPP.client.on("p", (p) => {
         document.getElementById(`namediv-${MPP.client.getOwnParticipant()._id}`).innerHTML = `<div class="nametag" id="nametag-${MPP.client.getOwnParticipant()._id}" style="background-color: rgb(${hexToRGB(tag.color)});">${tag.text}</div><div class="nametext" id="nametext-${MPP.client.getOwnParticipant()._id}">${MPP.client.getOwnParticipant().name}</div>`;
         document.getElementById(`namediv-${MPP.client.getOwnParticipant()._id}`).title = "This is a MPPCT user.";
     }
-    MPP.client.sendArray([{m: "custom", data: {m: 'mppct', tag: tag.text, color: tag.color}, target: { mode: 'subscribed', global: false } }]);
+    sendTag();
 });
 MPP.client.on("participant added", (p) => {
     if (p._id == MPP.client.getOwnParticipant()._id) {
@@ -62,11 +73,11 @@ MPP.client.on("participant added", (p) => {
         document.getElementById(`namediv-${MPP.client.getOwnParticipant()._id}`).title = "This is a MPPCT user.";
     }
     MPP.client.sendArray([{m: "custom", data: {m: 'mppctgt'}, target: { mode: 'subscribed', global: false } }]);
-    MPP.client.sendArray([{m: "custom", data: {m: 'mppct', tag: tag.text, color: tag.color}, target: { mode: 'subscribed', global: false } }]);
 });
- 
+
+
 // Buttons  //////////////////////////////////////////////////////////////////////////
-const a = document.createElement("input")
+const a = document.createElement("input");
 a.name = "tag";
 a.type = "text";
 a.placeholder = "Tag";
@@ -74,32 +85,32 @@ a.maxlength = "25";
 a.className = "text";
 a.style = "width: 100px; height: 20px;";
 document.body.getElementsByClassName("dialog").rename.appendChild(a);
- 
-const q = document.createElement("input")
+
+const q = document.createElement("input");
 q.name = "tagcolor";
 q.type = "color";
 q.placeholder = "";
 q.maxlength = "7";
 q.className = "color";
 document.body.getElementsByClassName("dialog").rename.appendChild(q);
- 
-const e = document.createElement("button")
+
+const e = document.createElement("button");
 e.addEventListener("click", () => {
     localStorage.tag = JSON.stringify({text: $("#rename input[name=tag]").val(), color: $("#rename input[name=tagcolor]").val()});
     tag = JSON.parse(localStorage.tag);
     document.getElementById(`namediv-${MPP.client.getOwnParticipant()._id}`).innerHTML = `<div class="nametag" id="nametag-${MPP.client.getOwnParticipant()._id}" style="background-color: rgb(${hexToRGB(tag.color)});">${tag.text}</div><div class="nametext" id="nametext-${MPP.client.getOwnParticipant()._id}">${MPP.client.getOwnParticipant().name}</div>`;
     document.getElementById(`namediv-${MPP.client.getOwnParticipant()._id}`).title = "This is a MPPCT user."
-    MPP.client.sendArray([{m: "custom", data: {m: 'mppct', tag: tag.text, color: tag.color}, target: { mode: 'subscribed', global: false } }]);
-})
+    sendTag()
+});
 e.innerText = "SET TAG";
 e.className = "submittag";
 e.style.position = "fixed";
 document.body.getElementsByClassName("dialog").rename.appendChild(e);
- 
+
 $("#rename input[name=tag]").val(tag.text);
 $("#rename input[name=tagcolor]").val(tag.color);
- 
- 
+
+
 //Version checker ////////////////////////////////
 fetch('https://raw.githubusercontent.com/Hyye123/MPPCT/main/version.json').then(r => r.json().then(json => {
     if (ver != json.latest) setInterval(function() {
@@ -114,5 +125,5 @@ fetch('https://raw.githubusercontent.com/Hyye123/MPPCT/main/version.json').then(
                 "id": "MPPCT"
             }
         });
-    }, 60000);
+    }, 30000);
 }));
